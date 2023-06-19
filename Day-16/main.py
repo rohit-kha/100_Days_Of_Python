89e1be6eb118ec69e5834556bf211183dbbc78e2
rohit <khadkarohit235@gmail.com>
2023-06-19 18:09:16 +0545

    Day-16 uploaded- OOP

---
+++ Day-16/main.py
@@ -0,0 +1,22 @@
+#
+# import another_module
+# # print(another_module.another_variable)
+#
+# from  turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# #changede the crusher to turtle shape
+# timmy.shape("turtle")
+# #Changed the color to red
+# timmy.color("red")
+# #Moved the turtle forward
+# timmy.forward(100)
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# #Scree goes off only on the click
+# my_screen.exitonclick()
+
+#Final Challenge
+#OOP=Coffee Machine
